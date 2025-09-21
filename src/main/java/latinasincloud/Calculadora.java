e4c4b2604b2ceebfaf46bd8167aa770e5cc7af43
CamilaCarrenoV <camila.carreno89@gmail.com>
2025-09-21 15:00:37 -0300

    feat: Creación del menú de calculadora

--- src/main/java/latinasincloud/Calculadora.java
+++ src/main/java/latinasincloud/Calculadora.java
@@ -1,4 +1,5 @@
 package latinasincloud;
+import java.util.Scanner;
 
 /*CALCULADORA JAVA
 Crear un progrmama que simule una calculadora con las siguientes funciones:
@@ -14,8 +15,23 @@ REQUISITOS
 * Menú Interactivo: Usar Scanner para pedir datos al Usuario.
 * GitHub: Subir el proyecto con commits claros. */
 
-public class Main {
+public class Calculadora {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0;
+        
 
+    do {
+        System.out.println("**Menú Calculadora**");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
+        System.out.println("5. Salir");
+        System.out.println("Elige una opción: ");
+        opcion = scanner.nextInt();
+
+
+    } while (opcion != 5);
     }
 }
